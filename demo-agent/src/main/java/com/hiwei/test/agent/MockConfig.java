2e470a83d33bac7509fc788177bc1e66031a1f05
zhw <wb-zhw771799@alibaba-inc.com>
2022-06-10 17:07:41 +0800

    style

--- demo-agent/src/main/java/com/hiwei/test/agent/MockConfig.java
+++ demo-agent/src/main/java/com/hiwei/test/agent/MockConfig.java
@@ -9,32 +9,28 @@ import java.util.Set;
 public class MockConfig {
     private static Map<String, Map<String, Object>> mockData = new HashMap<>();
 
-    public static String add(){
-        return "MockConfig";
-    }
-
-    public static void parseConfig(String configStr){
+    public static void parseConfig(String configStr) {
         JSONObject config = JSONObject.parseObject(configStr);
         Set<String> classNameSet = config.keySet();
-        classNameSet.forEach(className->{
-            Map<String,Object> methodMock = new HashMap<>();
+        classNameSet.forEach(className -> {
+            Map<String, Object> methodMock = new HashMap<>();
             JSONObject methods = config.getJSONObject(className);
             Set<String> mSet = methods.keySet();
-            mSet.forEach(m->{
-                methodMock.put(m,methods.get(m));
+            mSet.forEach(m -> {
+                methodMock.put(m, methods.get(m));
             });
-            mockData.put(className,methodMock);
+            mockData.put(className, methodMock);
         });
     }
 
-    public static boolean isMock(String className){
-        return !(mockData.get(className)==null);
+    public static boolean isMock(String className) {
+        return !(mockData.get(className) == null);
     }
 
 
-        public static Object getMockData(String className,String methodName){
+    public static Object getMockData(String className, String methodName) {
         Map<String, Object> stringJSONObjectMap = mockData.get(className);
-        if(stringJSONObjectMap!=null){
+        if (stringJSONObjectMap != null) {
             return stringJSONObjectMap.get(methodName);
         }
         return null;
